3834e0d9f905ac70412473ecd220ca25b9d5e676
robajillo <rjillo@yahoo.com>
2020-09-04 23:41:50 +0300

    used save_contact method that appends new user to user list

--- user.py
+++ user.py
@@ -9,3 +9,5 @@ class user:
     def__init__(self, user_name, password):
         self.user_name = user_name
         self.password = password
+    def save_user(self):
+        User.user_list.append(self)
