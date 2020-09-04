e6b69bb7393f8aed47eb707da959a853aab8860d
robajillo <rjillo@yahoo.com>
2020-09-04 23:02:51 +0300

    setUp method to run before each test case

--- user_test.py
+++ user_test.py
@@ -1,11 +1,10 @@
-'''
-Importing the unittest module
-'''
-from user import User
-import unittest
-'''
-Importing the user class
-'''
+import unittest  # Importing the unittest module
+from user import User  # Importing the user class
 
 
 class TestUser(unittest.TestCase):
+
+    def setUp(self)
+    '''
+        Set up method to run before each test cases.
+        '''
