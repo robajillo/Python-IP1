04321c425d1f7a90044681dc38474b44ef5ef88d
robajillo <rjillo@yahoo.com>
2020-09-05 16:20:50 +0300

    created a method to save new users

--- run.py
+++ run.py
@@ -1,2 +1,17 @@
 from credentials import Credentials
 from user import User
+
+def create_user(name,password):
+    '''
+    method to create new account
+    '''
+
+    new_user = User(name,password)
+
+    return new_user
+def save_users(user):
+    '''
+    method to save account
+    '''
+    user.save_user()  
+def      
