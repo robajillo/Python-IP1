dee214b8f025195cb24d9babf41788f4dd96aa2f
robajillo <rjillo@yahoo.com>
2020-09-05 18:09:43 +0300

    imported random module

--- run.py
+++ run.py
@@ -1,62 +1,63 @@
+import random
 from credentials import Credentials
 from user import User
 
-def create_useraccount(user_name,password):
-    '''
-    method to create user account
-    '''
+# def create_useraccount(user_name,password):
+#     '''
+#     method to create user account
+#     '''
 
-    new_user = User(user_name,password)
+#     new_user = User(user_name,password)
 
-    return new_user
-def save_user(user):
-    '''
-    method to save account
-    '''
-    user.save_user()  
-def save_credentials(credentials):
-    '''
-    method to save credentials account
-    '''
-    credentials.save_credentials
+#     return new_user
+# def save_user(user):
+#     '''
+#     method to save account
+#     '''
+#     user.save_user()  
+# def save_credentials(credentials):
+#     '''
+#     method to save credentials account
+#     '''
+#     credentials.save_credentials
 
-def find_user (user_name):
-    '''
-    method for finding user using user_name
-    '''
-    return User.find_user(user_name)
+# def find_user (user_name):
+#     '''
+#     method for finding user using user_name
+#     '''
+#     return User.find_user(user_name)
 
-def create_credentials(account, email, password):
-    '''
-    method to create credentials
-    '''
-    new_credentials = Crendentials(account, email, password) 
-    return new_credentials
+# def create_credentials(account, email, password):
+#     '''
+#     method to create credentials
+#     '''
+#     new_credentials = Crendentials(account, email, password) 
+#     return new_credentials
 
-def save_credentials(credentials):
-    '''
-    method to save credentials
-    '''
-    credentials.save_credentials()
+# def save_credentials(credentials):
+#     '''
+#     method to save credentials
+#     '''
+#     credentials.save_credentials()
 
-def display_credentials():
-    '''
-    method to display all credentials
-    '''
-    return Credentials.display_credentials()
+# def display_credentials():
+#     '''
+#     method to display all credentials
+#     '''
+#     return Credentials.display_credentials()
 
-def find_account(account):
-    '''
-    method to search for account 
-    '''
-    return Credentials.find_account(account)   
+# def find_account(account):
+#     '''
+#     method to search for account 
+#     '''
+#     return Credentials.find_account(account)   
     
-def delete_cred(account):
-    '''
-    method to delete account
-    '''
-    account.delete_credentials() 
-       
+# def delete_cred(account):
+#     '''
+#     method to delete account
+#     '''
+#     account.delete_credentials() 
+
 
 
     
